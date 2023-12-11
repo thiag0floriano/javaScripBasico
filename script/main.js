658c783ba549a97b3a83ef531ccfb6b6aa0b1c02
Thiago Floriano <110630909+thiag0floriano@users.noreply.github.com>
2023-12-11 18:12:11 -0300

    estudo1

---
+++ script/main.js
@@ -0,0 +1,30 @@
+const minhaImagem = document.querySelector("img");
+
+minhaImagem.onclick = () => {
+    const minhaSrc = minhaImagem.getAttribute("src");
+    if (minhaSrc === "images/firefox-icon.png"){
+        minhaImagem.setAttribute("src", "images/firefox-icon2.png");
+    }else{
+        minhaImagem.setAttribute("src", "images/firefox-icon.png");
+    }
+}
+
+let meubtn = document.querySelector("button");
+let meuTitulo = document.querySelector("h1");
+
+function setUsuario (){
+    const meuNome = prompt("Por favor, Digite seu nome");
+    localStorage.setItem("name", meuNome);
+    meuTitulo.textContent = `Mozilla é legal, ${meuNome}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUsuario();
+} else {
+    const storedName = localStorage.getItem("name");
+    meuTitulo.textContent = `Mozilla é legal, ${storedName}`;
+}
+
+meubtn.onclick = () => {
+    setUsuario();
+};
